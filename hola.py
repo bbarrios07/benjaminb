b96d3feadd87f90a1185882deb306de449cd1874
bbarrios07 <benjamin.barrios200710@gmail.com>
2025-04-15 15:38:28 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,20 @@
+
+# # declaracion de variables
+# nombre="Erika"
+# edad=64
+# #  ejemplo de concatenacion
+# print ("hola",nombre, "y su edad es",edad)
+
+# # declaracion de variables
+# print ("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+# #  ejemplo de concatenacion
+# print ("hola",nombre, "y su edad es",edad)
+
+
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+
+print("el resultado de la multiplicacion es," (n1*n2))
